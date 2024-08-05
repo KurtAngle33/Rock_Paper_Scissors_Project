cda95b712205b20353967b417d6098d02acf06de
KurtAngle33 <isae.rodriguez1@gmail.com>
2024-08-05 11:54:58 -0700

    Rewrote the human choice function so it returns a string value and began writing function to play a round

--- script.js
+++ script.js
@@ -14,10 +14,36 @@ console.log(getComputerChoice());
 // function for human's choice
 
 function getHumanChoice() {
+    let choice = "";
 
-}
+    choice = prompt("Rock, Paper, or Scissors?");
+    switch (choice.toLowerCase()) {
+        case "rock":
+        console.log(choice);
+        break;
+        case "paper":
+        console.log(choice);
+        break;
+        case "scissors":
+        console.log(choice);
+        break;
+        default:
+            console.log ("not a valid answer");
+    }
+    return choice;
+    }
+
 
 // function to play a round, using both choices
 
+function playRound(humanChoice, computerChoice) {
+}
+const humanSelection = getHumanChoice();
+console.log(humanSelection)
+const computerSelection = getComputerChoice();
+console.log(computerSelection)
+
+playRound(humanSelection, computerSelection);
 
 // run 5 total rounds 
+
